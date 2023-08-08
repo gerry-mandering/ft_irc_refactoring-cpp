de63d1b6716034da884c2902893cb92f048197d8
gerry-mandering <kimmen1119@gmail.com>
2023-08-08 15:09:40 +0900

    Feat: ClientRepository class

--- src/client_repository/ClientRepository.cpp
+++ src/client_repository/ClientRepository.cpp
@@ -1,3 +1,57 @@
-//
-// Created by Minseok Kim on 8/7/23.
-//
+#include "ClientRepository.hpp"
+
+shared_ptr< Client > ClientRepository::CreateClient(Socket socket)
+{
+    shared_ptr< Client > client = make_shared< Client >(socket);
+    mSocketToClientMap[socket] = client;
+
+    return client;
+}
+
+void ClientRepository::AddClientToNicknameMap(shared_ptr< Client > client)
+{
+    mNicknameToClientMap[client->GetNickname()] = client;
+}
+
+shared_ptr< Client > ClientRepository::FindBySocket(Socket socket)
+{
+    auto it = mSocketToClientMap.find(socket);
+    if (it != mSocketToClientMap.end())
+        return it->second;
+
+    return nullptr;
+}
+
+shared_ptr< Client > ClientRepository::FindByName(const string &name)
+{
+    auto it = mNicknameToClientMap.find(name);
+    if (it != mNicknameToClientMap.end())
+        return it->second;
+
+    return nullptr;
+}
+
+int ClientRepository::GetNumberOfClients() const
+{
+    return mSocketToClientMap.size();
+}
+
+void ClientRepository::RemoveClient(shared_ptr< Client > client)
+{
+    RemoveClientFromNicknameMap(client);
+
+    auto it = mSocketToClientMap.find(client->GetSocket());
+    if (it != mSocketToClientMap.end())
+        mSocketToClientMap.erase(it);
+
+    return;
+}
+
+void ClientRepository::RemoveClientFromNicknameMap(shared_ptr< Client > client)
+{
+    auto it = mNicknameToClientMap.find(client->GetNickname());
+    if (it != mNicknameToClientMap.end())
+        mNicknameToClientMap.erase(it);
+
+    return;
+}
