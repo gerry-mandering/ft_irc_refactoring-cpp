f4f891c12ac61b769afc5d3cc0204ef397d3dd0b
gerry-mandering <kimmen1119@gmail.com>
2023-08-10 18:52:46 +0900

    Feat: PrivmsgRequest class, PrivmsgRequestBuilder class

---
+++ src/request/PrivmsgRequest.cpp
@@ -0,0 +1,43 @@
+#include "PrivmsgRequest.hpp"
+
+bool PrivmsgRequest::Accept(visitor_pattern::Visitor *visitor)
+{
+    return visitor->Visit(this);
+}
+
+const std::vector< std::string > &PrivmsgRequest::GetTargets() const
+{
+    return mTargets;
+}
+
+const std::string &PrivmsgRequest::GetMessage() const
+{
+    return mMessage;
+}
+
+void PrivmsgRequest::SetTargets(const std::vector< std::string > &targets)
+{
+    mTargets = targets;
+}
+
+PrivmsgRequest::PrivmsgRequest(Socket socket, const std::vector< std::string > &targets, const std::string &message)
+    : Request(socket)
+{
+}
+
+PrivmsgRequestBuilder &PrivmsgRequestBuilder::SetTargets(const std::vector< std::string > &targets)
+{
+    mTargets = targets;
+    return *this;
+}
+
+PrivmsgRequestBuilder &PrivmsgRequestBuilder::SetMessage(const std::string &message)
+{
+    mMessage = message;
+    return *this;
+}
+
+Request *PrivmsgRequestBuilder::Build()
+{
+    return new PrivmsgRequest(mSocket, mTargets, mMessage);
+}
---
+++ src/request/PrivmsgRequest.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Request.hpp"
+#include "VisitorPattern.hpp"
+#include <string>
+#include <vector>
+
+class PrivmsgRequest : public Request, public visitor_pattern::Acceptor
+{
+  public:
+    bool Accept(visitor_pattern::Visitor *visitor) override;
+
+    const std::vector< std::string > &GetTargets() const;
+    const std::string &GetMessage() const;
+
+    void SetTargets(const std::vector< std::string > &targets);
+
+    friend class PrivmsgRequestBuilder;
+
+  private:
+    PrivmsgRequest(Socket socket, const std::vector< std::string > &targets, const std::string &message);
+
+  private:
+    std::vector< std::string > mTargets;
+    std::string mMessage;
+};
+
+class PrivmsgRequestBuilder : public RequestBuilder
+{
+  public:
+    PrivmsgRequestBuilder &SetTargets(const std::vector< std::string > &targets);
+    PrivmsgRequestBuilder &SetMessage(const std::string &message);
+
+    Request *Build() override;
+
+  private:
+    std::vector< std::string > mTargets;
+    std::string mMessage;
+};
