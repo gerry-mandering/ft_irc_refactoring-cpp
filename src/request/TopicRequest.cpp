ec40ae4f0c6c3d9cc44cc35a4f2d6a8440941d6f
gerry-mandering <kimmen1119@gmail.com>
2023-08-10 19:06:09 +0900

    Feat: TopicRequest class, TopicRequestBuilder class

---
+++ src/request/TopicRequest.cpp
@@ -0,0 +1,35 @@
+#include "TopicRequest.hpp"
+
+bool TopicRequest::Accept(visitor_pattern::Visitor *visitor)
+{
+    return visitor->Visit(this);
+}
+
+const std::string &TopicRequest::GetChannelname() const
+{
+    return mChannelname;
+}
+
+const std::string &TopicRequest::GetTopic() const
+{
+    return mTopic;
+}
+
+TopicRequest::TopicRequest(Socket socket, const std::string &channelname, const std::string &topic) : Request(socket) {}
+
+TopicRequestBuilder &TopicRequestBuilder::SetChannelname(const std::string &channelname)
+{
+    mChannelname = channelname;
+    return *this;
+}
+
+TopicRequestBuilder &TopicRequestBuilder::SetTopic(const std::string &topic)
+{
+    mTopic = topic;
+    return *this;
+}
+
+Request *TopicRequestBuilder::Build()
+{
+    return new TopicRequest(mSocket, mChannelname, mTopic);
+}
---
+++ src/request/TopicRequest.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Request.hpp"
+#include "VisitorPattern.hpp"
+#include <string>
+
+class TopicRequest : public Request, public visitor_pattern::Acceptor
+{
+  public:
+    bool Accept(visitor_pattern::Visitor *visitor) override;
+
+    const std::string &GetChannelname() const;
+    const std::string &GetTopic() const;
+
+    friend class TopicRequestBuilder;
+
+  private:
+    TopicRequest(Socket socket, const std::string &channelname, const std::string &topic);
+
+  private:
+    std::string mChannelname;
+    std::string mTopic;
+};
+
+class TopicRequestBuilder : public RequestBuilder
+{
+  public:
+    TopicRequestBuilder &SetChannelname(const std::string &channelname);
+    TopicRequestBuilder &SetTopic(const std::string &topic);
+
+    Request *Build() override;
+
+  private:
+    std::string mChannelname{};
+    std::string mTopic{};
+};
