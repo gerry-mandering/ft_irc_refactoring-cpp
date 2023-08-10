05f7bead1ab3cc7350f2069dd0acc99d58a2f788
gerry-mandering <kimmen1119@gmail.com>
2023-08-10 16:46:44 +0900

    Feat: NickRequest class, NickRequestBuilder class

---
+++ src/request/NickRequest.cpp
@@ -0,0 +1,24 @@
+#include "NickRequest.hpp"
+
+bool NickRequest::Accept(visitor_pattern::Visitor *visitor)
+{
+    return visitor->Visit(this);
+}
+
+const std::string &NickRequest::GetNickname() const
+{
+    return mNickname;
+}
+
+NickRequest::NickRequest(Socket socket, const std::string &nickname) : Request(socket) {}
+
+NickRequestBuilder &NickRequestBuilder::SetNickname(const std::string &nickname)
+{
+    mNickname = nickname;
+    return *this;
+}
+
+Request *NickRequestBuilder::Build()
+{
+    return new NickRequest(mSocket, mNickname);
+}
---
+++ src/request/NickRequest.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Request.hpp"
+#include "VisitorPattern.hpp"
+#include <string>
+
+class NickRequest : public Request, public visitor_pattern::Acceptor
+{
+  public:
+    bool Accept(visitor_pattern::Visitor *visitor) override;
+
+    const std::string &GetNickname() const;
+
+    friend class NickRequestBuilder;
+
+  private:
+    NickRequest(Socket socket, const std::string &nickname);
+
+  private:
+    std::string mNickname;
+};
+
+class NickRequestBuilder : public RequestBuilder
+{
+  public:
+    NickRequestBuilder &SetNickname(const std::string &nickname);
+
+    Request *Build() override;
+
+  private:
+    std::string mNickname;
+};
