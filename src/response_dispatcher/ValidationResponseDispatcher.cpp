dbd72fbd13e2939d3a88de9b0b2c1ddc8c3e2a79
gerry-mandering <kimmen1119@gmail.com>
2023-08-17 21:41:12 +0900

    Feat: ValidationResponseDispatcher class implementation

--- src/response_dispatcher/ValidationResponseDispatcher.cpp
+++ src/response_dispatcher/ValidationResponseDispatcher.cpp
@@ -8,3 +8,43 @@ void ValidationResponseDispatcher::DispatchNotRegisteredMsg(const NotRegisteredD
 
     dto.GetRecipient()->AddResponse(message);
 }
+
+void ValidationResponseDispatcher::DispatchNoSuchChannelMsg(const NoSuchChannelDto &dto) const
+{
+    std::string message = buildMessage(
+        {getServerName(), dto.GetErrorCode(), dto.GetNickname(), dto.GetChannelname(), dto.GetDescription()});
+
+    dto.GetRecipient()->AddResponse(message);
+}
+
+void ValidationResponseDispatcher::DispatchNoSuchNickMsg(const NoSuchNickDto &dto) const
+{
+    std::string message = buildMessage(
+        {getServerName(), dto.GetErrorCode(), dto.GetNickname(), dto.GetTargetNickname(), dto.GetDescription()});
+
+    dto.GetRecipient()->AddResponse(message);
+}
+
+void ValidationResponseDispatcher::DispatchNotOnChannelMsg(const NotOnChannelDto &dto) const
+{
+    std::string message = buildMessage(
+        {getServerName(), dto.GetErrorCode(), dto.GetNickname(), dto.GetChannelname(), dto.GetDescription()});
+
+    dto.GetRecipient()->AddResponse(message);
+}
+
+void ValidationResponseDispatcher::DispatchUserOnChannelMsg(const UserOnChannelDto &dto) const
+{
+    std::string message = buildMessage({getServerName(), dto.GetErrorCode(), dto.GetNickname(), dto.GetTargetNickname(),
+                                        dto.GetChannelname(), dto.GetDescription()});
+
+    dto.GetRecipient()->AddResponse(message);
+}
+
+void ValidationResponseDispatcher::DispatchNotChannelOperatorMsg(const NotChannelOperatorDto &dto) const
+{
+    std::string message = buildMessage(
+        {getServerName(), dto.GetErrorCode(), dto.GetNickname(), dto.GetChannelname(), dto.GetDescription()});
+
+    dto.GetRecipient()->AddResponse(message);
+}
--- src/response_dispatcher/ValidationResponseDispatcher.hpp
+++ src/response_dispatcher/ValidationResponseDispatcher.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#include "NoSuchChannelDto.hpp"
+#include "NoSuchNickDto.hpp"
+#include "NotChannelOperatorDto.hpp"
+#include "NotOnChannelDto.hpp"
 #include "NotRegisteredDto.hpp"
 #include "ResponseDispatcher.hpp"
+#include "UserOnChannelDto.hpp"
 
 class ValidationResponseDispatcher : public ResponseDispatcher
 {
   public:
     void DispatchNotRegisteredMsg(const NotRegisteredDto &dto) const;
+    void DispatchNoSuchChannelMsg(const NoSuchChannelDto &dto) const;
+    void DispatchNoSuchNickMsg(const NoSuchNickDto &dto) const;
+    void DispatchNotOnChannelMsg(const NotOnChannelDto &dto) const;
+    void DispatchUserOnChannelMsg(const UserOnChannelDto &dto) const;
+    void DispatchNotChannelOperatorMsg(const NotChannelOperatorDto &dto) const;
 };
